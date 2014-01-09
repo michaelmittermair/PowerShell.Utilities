65e33e5b5e29380eccd9713d272cf0c6d2e7dd08
Michael Mittermair <michael.mittermair@epunkt.net>
2014-01-09 07:36:10 +0100

    added script to create a windows schedule task to run the update script every day at 05:00am

---
+++ Windows Updates/Schedule Windows Updates.ps1
@@ -0,0 +1,36 @@
+﻿function Create-Windows-Update-Task {
+    param($name, $program, $programArguments)
+    
+    $service = New-Object -ComObject "Schedule.Service"
+    $service.Connect($ENV:ComputerName)
+
+    $rootFolder = $service.GetFolder("\")
+    $taskDefinition = $service.NewTask(0)
+
+    $regInfo = $taskDefinition.RegistrationInfo
+    $regInfo.Description = 'Windows Update - start at 05:00 daily'
+    $regInfo.Author = $taskRunAsuser
+    $settings = $taskDefinition.Settings
+    $settings.Enabled = $True
+    $settings.StartWhenAvailable = $True
+    $settings.Hidden = $False
+      
+    $trigger = $taskDefinition.Triggers.Create(2)
+    $trigger.StartBoundary = (Get-Date 05:00AM).AddDays(1) | Get-Date -Format yyyy-MM-ddTHH:ss:ms
+    $trigger.DaysInterval = 1
+    $trigger.Id = "DailyTriggerId"
+    $trigger.Enabled = $True
+   
+    $action = $taskDefinition.Actions.Create(0)
+    $action.Path = $program
+    $action.Arguments = $programArguments
+    
+    $principal = $taskDefinition.Principal
+    $principal.RunLevel = 1 # 0=normal, 1=Highest Privileges
+       
+    $rootFolder.RegisterTaskDefinition($name, $taskDefinition, 2, "System", $null , 5)
+}
+
+$filePath = "c:\Update\WindowsUpdateViaPS.ps1"
+
+Create-Windows-Update-Task ("Windows Update") ("powershell") ("-file " + $filePath)
