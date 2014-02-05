dfa39608cb96c7829885e92f0c1c947bf98a450c
Michael Mittermair <michael.mittermair@epunkt.net>
2014-02-05 08:52:44 +0100

    added removing network drives to mapping script

--- MapNetworkDrive.ps1
+++ MapNetworkDrive.ps1
@@ -1,10 +1,18 @@
-$Username = Read-Host "Benutzername: "
-$Password = Read-Host "Password: "
+param([string]$action)
 
 $RemotePath = "\\Server1\C$"
 $LocalPath = "Z:"
 
-#adding in here
-$net = New-Object -com WScript.Network
-$net.mapnetworkdrive($LocalPath, $RemotePath, "false", $Username, $Password)
- 
+
+if($action -eq "remove"){
+    #adding in here
+    $net = New-Object -com WScript.Network
+    $net.RemoveNetworkDrive($LocalPath, "TRUE","TRUE")
+} else {
+    $Username = Read-Host "Benutzername: "
+    $Password = Read-Host "Password: "
+
+    #adding in here
+    $net = New-Object -com WScript.Network
+    $net.MapNetworkDrive($LocalPath, $RemotePath, "false", $Username, $Password)
+}
