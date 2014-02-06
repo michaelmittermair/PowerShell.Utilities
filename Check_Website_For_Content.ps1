22212a19656b3a8f76b6479b70632f8d86e7aa78
Michael Mittermair <michael.mittermair@epunkt.net>
2014-02-06 10:46:04 +0100

    added script to check the content of a website for special text blocks

---
+++ Check_Website_For_Content.ps1
@@ -0,0 +1,27 @@
+﻿$SMTPServer = "SMTP-Server" #ie. mail.gmx.net
+$username = "username"
+$password = ConvertTo-SecureString "password" -AsPlainText -Force
+$outputFile = "D:\output.html"
+
+$Sender = "Sender <username@domain>"
+$Recipient = "Recipient <username@domain>"
+$Subject = "Subject" 
+$Body = "Body Mail" 
+$creds = New-Object System.Management.Automation.PSCredential($username, $password)
+
+
+$URL = "http://url_to_check"
+$ie = New-Object -com InternetExplorer.Application
+$ie.visible=$true
+$ie.navigate($URL) 
+while($ie.ReadyState -ne 4) {start-sleep -m 100} # waiting until the application has started
+
+$ie.Document.body.outerHTML | Out-File -FilePath $outputFile # getting the content and saving this to the local drive
+$ie.Quit()
+
+$text = Get-Content $outputFile -Raw 
+
+if($text.Contains("asdf")) # check the content
+{
+    Send-MailMessage -To $Recipient -From $Sender -Subject $Subject -Body $Body -SmtpServer $SMTPServer -Credential $creds -encoding ([System.Text.Encoding]::UTF8) -UseSsl
+}
